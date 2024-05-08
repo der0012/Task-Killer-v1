7e265779fb975a2b36e180959e8ec457fc5d4cb4
der0012 <der0012@roxburghcollege.vic.edu.au>
2024-05-08 10:32:16 +1000

    Add files via upload

---
+++ TaskEnd.bat
@@ -0,0 +1,39 @@
+echo off
+:top
+cls
+color 02
+echo Task Killer V1
+echo.
+echo.
+echo 1 - Task Killer V1
+echo 2 - Task List w/ Task Killer v1
+set /p which="Enter>"
+if %which%==1 goto taskkiller
+if %which%==2 goto tasklist
+:taskkiller
+echo Type in Task name (i.e explorer.exe, dwm.exe, isass.exe) to kill
+set /p input="ToKill>"
+taskkill /im %input%
+echo Task Killed!
+echo.
+goto top
+pause
+:tasklist
+tasklist
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo Type in Task name (i.e explorer.exe, dwm.exe, isass.exe) to kill
+set /p input="ToKill>"
+taskkill /im %input%
+echo Task Killed!
+echo.
+goto top
+pause
