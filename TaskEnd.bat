4209363d15b50a4655eb0dd71bf811e5cac192b8
der0012 <der0012@roxburghcollege.vic.edu.au>
2024-05-08 10:36:12 +1000

    Update TaskEnd.bat

--- TaskEnd.bat
+++ TaskEnd.bat
@@ -7,15 +7,16 @@ echo.
 echo.
 echo 1 - Task Killer V1
 echo 2 - Task List w/ Task Killer v1
-set /p which="Enter>"
+set /p which="Enter> "
 if %which%==1 goto taskkiller
 if %which%==2 goto tasklist
 :taskkiller
 echo Type in Task name (i.e explorer.exe, dwm.exe, isass.exe) to kill
-set /p input="ToKill>"
+set /p input="ToKill >"
 taskkill /im %input%
-echo Task Killed!
+echo 
 echo.
+pause
 goto top
 pause
 :tasklist
